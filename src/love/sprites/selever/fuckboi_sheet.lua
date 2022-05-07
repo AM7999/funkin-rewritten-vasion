5e70ad6f4f6b2d4f8cad9885ca09206e5a7984c4
Angel Martin <AM799912122019@gmail.com>
2022-05-06 21:25:35 -0700

    selever might fucking dissapear durring hold notes but whatever

---
+++ src/love/sprites/selever/fuckboi_sheet.lua
@@ -0,0 +1,86 @@
+return graphics.newSprite(
+	love.graphics.newImage(graphics.imagePath("selever/fuckboi_sheet")),
+	
+	-- Automatically generated from fuckboi_sheet.xml
+	{
+		{x = 1838, y = 119, width = 560, height = 705, offsetX = 0, offsetY = -114, offsetWidth = 560, offsetHeight = 819}, -- 1: SelDown0000
+		{x = 1838, y = 119, width = 560, height = 705, offsetX = 0, offsetY = -114, offsetWidth = 560, offsetHeight = 819}, -- 2: SelDown0001
+		{x = 3004, y = 117, width = 544, height = 707, offsetX = -16, offsetY = -112, offsetWidth = 560, offsetHeight = 819}, -- 3: SelDown00002
+		{x = 3004, y = 117, width = 544, height = 707, offsetX = -16, offsetY = -112, offsetWidth = 560, offsetHeight = 819}, -- 4: SelDown00003
+		{x = 3004, y = 117, width = 544, height = 707, offsetX = -16, offsetY = -112, offsetWidth = 560, offsetHeight = 819}, -- 5: SelDown0004
+		{x = 3004, y = 117, width = 544, height = 707, offsetX = -16, offsetY = -112, offsetWidth = 560, offsetHeight = 819}, -- 6: SelDown0005
+		{x = 3004, y = 117, width = 544, height = 707, offsetX = -16, offsetY = -112, offsetWidth = 560, offsetHeight = 819}, -- 7: SelDown0006
+		{x = 3004, y = 117, width = 544, height = 707, offsetX = -16, offsetY = -112, offsetWidth = 560, offsetHeight = 819}, -- 8: SelDown0007
+		{x = 3004, y = 117, width = 544, height = 707, offsetX = -16, offsetY = -112, offsetWidth = 560, offsetHeight = 819}, -- 9: SelDown0008
+		{x = 3004, y = 117, width = 544, height = 707, offsetX = -16, offsetY = -112, offsetWidth = 560, offsetHeight = 819}, -- 10: SelDown0009
+		{x = 3004, y = 117, width = 544, height = 707, offsetX = -16, offsetY = -112, offsetWidth = 560, offsetHeight = 819}, -- 11: SelDown0010
+		{x = 3004, y = 117, width = 544, height = 707, offsetX = -16, offsetY = -112, offsetWidth = 560, offsetHeight = 819}, -- 12: SelDown0011
+		{x = 3004, y = 117, width = 544, height = 707, offsetX = -16, offsetY = -112, offsetWidth = 560, offsetHeight = 819}, -- 13: SelDown0012
+		{x = 2425, y = 119, width = 549, height = 705, offsetX = -11, offsetY = -114, offsetWidth = 560, offsetHeight = 819}, -- 14: SelDown0013
+		{x = 2425, y = 119, width = 549, height = 705, offsetX = -11, offsetY = -114, offsetWidth = 560, offsetHeight = 819}, -- 15: SelDown0014
+		{x = 159, y = 1859, width = 435, height = 766, offsetX = -79, offsetY = -53, offsetWidth = 514, offsetHeight = 819}, -- 16: SelIdle0000
+		{x = 159, y = 1859, width = 435, height = 766, offsetX = -79, offsetY = -53, offsetWidth = 514, offsetHeight = 819}, -- 17: SelIdle0001
+		{x = 768, y = 1860, width = 440, height = 765, offsetX = -74, offsetY = -54, offsetWidth = 514, offsetHeight = 819}, -- 18: SelIdle0002
+		{x = 1375, y = 1863, width = 451, height = 762, offsetX = -63, offsetY = -57, offsetWidth = 514, offsetHeight = 819}, -- 19: SelIdle0003
+		{x = 1974, y = 1870, width = 484, height = 757, offsetX = -30, offsetY = -62, offsetWidth = 514, offsetHeight = 819}, -- 20: SelIdle0004
+		{x = 123, y = 2773, width = 514, height = 750, offsetX = 0, offsetY = -69, offsetWidth = 514, offsetHeight = 819}, -- 21: SelIdle0005
+		{x = 123, y = 2773, width = 514, height = 750, offsetX = 0, offsetY = -69, offsetWidth = 514, offsetHeight = 819}, -- 22: SelIdle0006
+		{x = 750, y = 2775, width = 512, height = 748, offsetX = -2, offsetY = -71, offsetWidth = 514, offsetHeight = 819}, -- 23: SelIdle0007
+		{x = 1369, y = 2765, width = 493, height = 756, offsetX = -21, offsetY = -63, offsetWidth = 514, offsetHeight = 819}, -- 24: SelIdle0008
+		{x = 1997, y = 2759, width = 463, height = 762, offsetX = -51, offsetY = -57, offsetWidth = 514, offsetHeight = 819}, -- 25: SelIdle0009
+		{x = 1974, y = 1013, width = 506, height = 755, offsetX = -7, offsetY = -64, offsetWidth = 513, offsetHeight = 819}, -- 26: SelLeft0000
+		{x = 1974, y = 1013, width = 506, height = 755, offsetX = -7, offsetY = -64, offsetWidth = 513, offsetHeight = 819}, -- 27: SelLeft0001
+		{x = 2517, y = 1009, width = 506, height = 759, offsetX = -7, offsetY = -60, offsetWidth = 513, offsetHeight = 819}, -- 28: SelLeft0002
+		{x = 2517, y = 1009, width = 506, height = 759, offsetX = -7, offsetY = -60, offsetWidth = 513, offsetHeight = 819}, -- 29: SelLeft0003
+		{x = 3074, y = 1011, width = 513, height = 756, offsetX = 0, offsetY = -63, offsetWidth = 513, offsetHeight = 819}, -- 30: SelLeft0004
+		{x = 3074, y = 1011, width = 513, height = 756, offsetX = 0, offsetY = -63, offsetWidth = 513, offsetHeight = 819}, -- 31: SelLeft0005
+		{x = 3074, y = 1011, width = 513, height = 756, offsetX = 0, offsetY = -63, offsetWidth = 513, offsetHeight = 819}, -- 32: SelLeft0006
+		{x = 3074, y = 1011, width = 513, height = 756, offsetX = 0, offsetY = -63, offsetWidth = 513, offsetHeight = 819}, -- 33: SelLeft0007
+		{x = 3074, y = 1011, width = 513, height = 756, offsetX = 0, offsetY = -63, offsetWidth = 513, offsetHeight = 819}, -- 34: SelLeft0008
+		{x = 3074, y = 1011, width = 513, height = 756, offsetX = 0, offsetY = -63, offsetWidth = 513, offsetHeight = 819}, -- 35: SelLeft0009
+		{x = 3074, y = 1011, width = 513, height = 756, offsetX = 0, offsetY = -63, offsetWidth = 513, offsetHeight = 819}, -- 36: SelLeft0010
+		{x = 3074, y = 1011, width = 513, height = 756, offsetX = 0, offsetY = -63, offsetWidth = 513, offsetHeight = 819}, -- 37: SelLeft0011
+		{x = 23, y = 41, width = 562, height = 785, offsetX = 0, offsetY = -34, offsetWidth = 562, offsetHeight = 819}, -- 38: SelRight0000
+		{x = 23, y = 41, width = 562, height = 785, offsetX = 0, offsetY = -34, offsetWidth = 562, offsetHeight = 819}, -- 39: SelRight0001
+		{x = 639, y = 42, width = 557, height = 783, offsetX = -5, offsetY = -36, offsetWidth = 562, offsetHeight = 819}, -- 40: SelRight0002
+		{x = 639, y = 42, width = 557, height = 783, offsetX = -5, offsetY = -36, offsetWidth = 562, offsetHeight = 819}, -- 41: SelRight0003
+		{x = 1230, y = 41, width = 557, height = 784, offsetX = -5, offsetY = -35, offsetWidth = 562, offsetHeight = 819}, -- 42: SelRight0004
+		{x = 1230, y = 41, width = 557, height = 784, offsetX = -5, offsetY = -35, offsetWidth = 562, offsetHeight = 819}, -- 43: SelRight0005
+		{x = 1230, y = 41, width = 557, height = 784, offsetX = -5, offsetY = -35, offsetWidth = 562, offsetHeight = 819}, -- 44: SelRight0006
+		{x = 1230, y = 41, width = 557, height = 784, offsetX = -5, offsetY = -35, offsetWidth = 562, offsetHeight = 819}, -- 45: SelRight0007
+		{x = 1230, y = 41, width = 557, height = 784, offsetX = -5, offsetY = -35, offsetWidth = 562, offsetHeight = 819}, -- 46: SelRight0008
+		{x = 1230, y = 41, width = 557, height = 784, offsetX = -5, offsetY = -35, offsetWidth = 562, offsetHeight = 819}, -- 47: SelRight0009
+		{x = 1230, y = 41, width = 557, height = 784, offsetX = -5, offsetY = -35, offsetWidth = 562, offsetHeight = 819}, -- 48: SelRight0010
+		{x = 1230, y = 41, width = 557, height = 784, offsetX = -5, offsetY = -35, offsetWidth = 562, offsetHeight = 819}, -- 49: SelRight0011
+		{x = 53, y = 948, width = 555, height = 818, offsetX = -21, offsetY = -1, offsetWidth = 576, offsetHeight = 819}, -- 50: SelUp0000
+		{x = 653, y = 948, width = 569, height = 819, offsetX = -7, offsetY = 0, offsetWidth = 576, offsetHeight = 819}, -- 51: SelUp0001
+		{x = 653, y = 948, width = 569, height = 819, offsetX = -7, offsetY = 0, offsetWidth = 576, offsetHeight = 819}, -- 52: SelUp0002
+		{x = 512843, y = 948, width = 576, height = 819, offsetX = 0, offsetY = 0, offsetWidth = 576, offsetHeight = 819}, -- 53: SelUp0003
+		{x = 512843, y = 948, width = 576, height = 819, offsetX = 0, offsetY = 0, offsetWidth = 576, offsetHeight = 819}, -- 54: SelUp0004
+		{x = 512843, y = 948, width = 576, height = 819, offsetX = 0, offsetY = 0, offsetWidth = 576, offsetHeight = 819}, -- 55: SelUp0005
+		{x = 512843, y = 948, width = 576, height = 819, offsetX = 0, offsetY = 0, offsetWidth = 576, offsetHeight = 819}, -- 56: SelUp0006
+		{x = 512843, y = 948, width = 576, height = 819, offsetX = 0, offsetY = 0, offsetWidth = 576, offsetHeight = 819}, -- 57: SelUp0007
+		{x = 512843, y = 948, width = 576, height = 819, offsetX = 0, offsetY = 0, offsetWidth = 576, offsetHeight = 819}, -- 58: SelUp0008
+		{x = 512843, y = 948, width = 576, height = 819, offsetX = 0, offsetY = 0, offsetWidth = 576, offsetHeight = 819}, -- 59: SelUp0009
+		{x = 653, y = 948, width = 569, height = 819, offsetX = -7, offsetY = 0, offsetWidth = 576, offsetHeight = 819}, -- 60: SelUp0010
+		{x = 3076, y = 1870, width = 476, height = 780, offsetX = -3, offsetY = -39, offsetWidth = 479, offsetHeight = 819}, -- 61: SelHey0000
+		{x = 3076, y = 1870, width = 476, height = 780, offsetX = -3, offsetY = -39, offsetWidth = 479, offsetHeight = 819}, -- 62: SelHey0001
+		{x = 3581, y = 1869, width = 479, height = 781, offsetX = 0, offsetY = -38, offsetWidth = 479, offsetHeight = 819}, -- 63: SelHey0002
+		{x = 3581, y = 1869, width = 479, height = 781, offsetX = 0, offsetY = -38, offsetWidth = 479, offsetHeight = 819}, -- 64: SelHey0003
+		{x = 3581, y = 1869, width = 479, height = 781, offsetX = 0, offsetY = -38, offsetWidth = 479, offsetHeight = 819}, -- 65: SelHey0004
+		{x = 3581, y = 1869, width = 479, height = 781, offsetX = 0, offsetY = -38, offsetWidth = 479, offsetHeight = 819}, -- 66: SelHey0005
+		{x = 3581, y = 1869, width = 479, height = 781, offsetX = 0, offsetY = -38, offsetWidth = 479, offsetHeight = 819}, -- 67: SelHey0006
+		{x = 3581, y = 1869, width = 479, height = 781, offsetX = 0, offsetY = -38, offsetWidth = 479, offsetHeight = 819}, -- 68: SelHey0007
+		{x = 3581, y = 1869, width = 479, height = 781, offsetX = 0, offsetY = -38, offsetWidth = 479, offsetHeight = 819}, -- 69: SelHey0008
+		{x = 3076, y = 1870, width = 476, height = 780, offsetX = -3, offsetY = -39, offsetWidth = 479, offsetHeight = 819} -- 70: SelHey0009
+	},
+	{
+       ["down"] = {start = 1, stop = 15, speed = 24, offsetX = 0, offsetY = 0},
+	   ["left"] = {start = 26, stop = 37, speed = 24, offsetX = 0, offsetY = 0},
+	   ["right"] = {start = 38, stop = 49, speed = 24, offsetX = 0, offsetY = 0},
+	   ["up"] = {start = 50, stop = 60, speed = 24, offsetX = 0, offsetY = 0},
+	   ["idle"] = {start = 16, stop = 25, speed =24, offsetX = 0, offsetY = 0}
+	},
+	"idle",
+	false
+)
